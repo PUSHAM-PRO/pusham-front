000be7ed6fd80aedebcf8625c2a03fb1512a12f4
Etheldra Akahoho <etheldeak4@gmail.com>
2024-11-04 10:32:45 +0000

    Created the customer facing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import './App.css'
-import CustomerDashboard from './layouts/CustomerDashboard'
+import SideBar from './components/SideBar';
+import CustomerFacing from './pages/customerpage/CustomerFacing';
+
 
 
 function App() {
   const router = createBrowserRouter([
-        {
-          path: "/dashboard",
-          element: <CustomerDashboard />,
-        }
-      ]
+    {
+      path: "/dashboard",
+      element: <CustomerFacing />,
+    },
+    {
+      path: "/sidebar",
+      element: <SideBar />,
+    },
+  ]
   );
 
   return <RouterProvider router={router} />;
-}
+};
 
 export default App;
