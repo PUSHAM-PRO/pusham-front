8d4f5c804039a3359c383e6e334f7d8e45665256
tekorMEST <marteymartekor55@gmail.com>
2024-11-11 14:46:51 +0000

    created services

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import EditENEOAccountForm from './pages/superadmin/EditAccountform';
 import EneoDepartmentManagement from './pages/eneodepartment/EneoDepartmentManagement';
 import AgentSignUp from './pages/signups/AgentSignUp';
 import CustomerSignUp from './pages/signups/CustomerSignUp';
+import TenderSubmissionForm from './pages/tendersub';
 
 function App() {
   const router = createBrowserRouter([
@@ -60,6 +61,11 @@ function App() {
       path: "/admin/eneo",
       element: <EneoDepartmentManagement />
     },
+    {
+      path: "/tender",
+      element: <TenderSubmissionForm />
+    },
+    
     
 
   ]);
