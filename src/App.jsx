e20360a2796985f5e125a2ecefba594baae84b82
Ms-Adubea <apeadutheodora@gmail.com>
2024-11-12 14:22:42 +0000

    Added Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import EneoDepartmentManagement from './pages/eneodepartment/EneoDepartmentManag
 import AgentSignUp from './pages/signups/AgentSignUp';
 import CustomerSignUp from './pages/signups/CustomerSignUp';
 import TenderSubmissionForm from './pages/tendersub';
-
 import TicketCreation from './pages/customerpage/TicketCreation';
 import StatusTracking from './pages/customerpage/StatusTracking';
 import EditTicket from './pages/customerpage/EditTicket';
@@ -19,9 +18,34 @@ import DepartmentDashboard from './layouts/DepartmentDashboard';
 import CreateTicket from './pages/departmentDashboard/createTicket';
 import ShareTicketForm from './pages/departmentDashboard/ShareTicketForm';
 import TicketDetails from './pages/departmentDashboard/ticketDetails';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken } from "firebase/messaging";
+import { useEffect } from 'react';
+
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxSlWotU1ly1O0zeyvEJK-oeBjA1m4is8",
+  authDomain: "pushanotification.firebaseapp.com",
+  projectId: "pushanotification",
+  storageBucket: "pushanotification.firebasestorage.app",
+  messagingSenderId: "752431191312",
+  appId: "1:752431191312:web:dc2455b78896f3f9034195",
+  measurementId: "G-TZWMT5CWHG"
+};
+
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
 
 
 function App() {
+useEffect (() => {
+  // Add the public key generated from the console here.
+getToken(messaging, {vapidKey: "BCZ6z1ijhgwO5DZupmFHhQ7rpjzixz6mZbCdo478Fo_2a1jgo7AoxjQTTl9lWcSml9VPQ7uNOT9FkLIij52T84A"});
+}, []);
+
   const router = createBrowserRouter([
     {
       path: '/login',
