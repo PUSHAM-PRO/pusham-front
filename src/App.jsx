6dc702d280b7c8a38bbceb2deeb5621d5187c797
Etheldra Akahoho <etheldeak4@gmail.com>
2024-11-19 16:14:41 +0000

    Updates made to status tracking

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ import AgentCreateTicket from './pages/agent/AgentCreateTicket';
 import AgentTicketDetails from './pages/agent/AgentTicketDetails';
 import EscalateModal from './components/modals/EscalateModal';
 import CustomerTracking from './pages/superadmin/CustomerTracking';
+import CustomerTicketDetails from './pages/customerpage/CustomerTicketDetails';
 
 
 function App() {
@@ -83,6 +84,10 @@ function App() {
     {
       path: "/status-tracking",
       element: <StatusTracking />,
+    },
+    {
+      path: "/custtickdetails",
+      element: <CustomerTicketDetails />,
     },
     {
       path: "/edit",
